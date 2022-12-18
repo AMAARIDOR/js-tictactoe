6e0e598def2cf21c2ced5019db8171d084304677
HZZRD <arish2k3@gmail.com>
2022-12-18 15:36:00 +0530

    add player switch function without logic and override

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+let playerSwitch = 1;
+
+document.querySelectorAll("[data-cell]").forEach((e) =>
+  e.addEventListener("click", () => {
+    const gridItemValue = e.textContent;
+
+    if (playerSwitch === 1) {
+      gridItemValue = "X";
+      playerSwitch = 0;
+    } else {
+      gridItemValue = "O";
+      playerSwitch = 1;
+    }
+  })
+);
