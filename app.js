67ee75e057ea8ab734b18855c39e92ea40c15f5c
HZZRD <arish2k3@gmail.com>
2022-12-18 15:55:51 +0530

    add reset and start game button functionality

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 "use strict";
 
 let playerSwitch = 1;
+let gameStartButtonElement = document.getElementById("gameStart");
+let allGridItems = document.querySelectorAll(".cell");
 
-document.querySelectorAll("[data-cell]").forEach((e) =>
-  e.addEventListener("click", () => {
-    if (playerSwitch === 1) {
-      e.textContent = "X";
-      playerSwitch = 0;
-    } else {
-      e.textContent = "O";
-      playerSwitch = 1;
-    }
-  })
-);
+let handleClicks = () => {
+  gameStartButtonElement.textContent = "Reset";
+
+  document.querySelectorAll("[data-cell]").forEach((e) =>
+    e.addEventListener("click", () => {
+      gameStartButtonElement.addEventListener("click", () => {
+        allGridItems.forEach((d) => (d.textContent = "."));
+      });
+
+      if (playerSwitch === 1) {
+        e.textContent = "X";
+        playerSwitch = 0;
+      } else {
+        e.textContent = "O";
+        playerSwitch = 1;
+      }
+    })
+  );
+};
+
+gameStartButtonElement.addEventListener("click", handleClicks);
